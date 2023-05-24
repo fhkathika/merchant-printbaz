59322479cb964149acb46a266cdd0beaf978e016
Shuvroprintbaz <shuvro.printbaz@gmail.com>
2023-05-24 13:39:28 +0600

    host into cpanel

--- src/Component/login/Login.jsx
+++ src/Component/login/Login.jsx
@@ -61,7 +61,7 @@ const handleSubmit = async (e) => {
     email: email,
     password: password
   };
-
+  
   try {
     const response = await fetch('http://localhost:5000/login', {
       method: 'POST',

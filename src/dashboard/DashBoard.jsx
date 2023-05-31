4a4dac4a4f572bf69eaa77cc213bfd90a60eecfc
Shuvroprintbaz <shuvro.printbaz@gmail.com>
2023-05-31 11:16:52 +0600

    added video in dashboard

--- src/dashboard/DashBoard.jsx
+++ src/dashboard/DashBoard.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Login from '../Component/login/Login';
 import Register from '../Component/login/Register';
 import NavigationBar from '../Component/Navbar/NavigationBar';
+import YoutubeEmbed from '../Component/youtubeEmbaded/YoutubeEmbaded';
 import { AuthContext } from '../context/AuthProvider/AuthProvider';
 import  "../css/styles.css"
 import { useGetData } from '../hooks/useGetData';
@@ -98,12 +99,26 @@ let dueAmount=parseInt(totalReceiveBase-statusPaidbase)
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
      <style dangerouslySetInnerHTML={{__html: "\n      /* General styles */\nbody {\n  font-family: Arial, sans-serif;\n  background-color: #f8f9fa;\n}\n\n.navbar {\n  background-color: #001846 !important;\n  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\n  padding: 20px;\n  padding-left: 40px !important;\n}\n\n.navbar-brand img {\n  width: 150px;\n}\n\n.nav-link {\n  color: #ffffff !important;\n  font-size: 16px;\n  font-weight: 600;\n}\n\n.nav-link:hover {\n  background-color: #ffffff;\n  color: #001846 !important;\n}\n.dropdown{\n  padding-left: 1200px;\n}\n\n.dropdown-menu {\n  margin-left: 1120px;\n  \n}\n\n.container {\n  max-width: 1200px;\n}\n\n/* Dashboard */\n.dashboard-title {\n  font-weight: 800 !important;\n  font-size: 50px !important;\n  color: #001846;\n  margin-bottom: 1rem;\n  text-transform: uppercase;\n}\n\n.dashboard-card {\n  background-color: #ffffff;\n  border-radius: 0.25rem;\n  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\n  padding: 1.5rem;\n  height: 100%;\n  padding-bottom: 10px !important;\n}\n\n.dashboard-card h3 {\n  font-size: 30px;\n  font-weight: 700;\n  color: #001846;\n  text-transform: uppercase;\n}\n\n.dashboard-card h4 {\n  font-size: 25spx;\n  font-weight: 700;\n  color: #001846;\n  text-transform: uppercase;\n}\n\n.dashboard-card p {\n  font-size: 15px;\n  font-weight: 600;\n  color: #6b6b6b;\n}\n\n.Payment-btn button {\n  background-color: rgb(234, 58, 59);\n  border: none;\n  font-weight: 700;\n}\n\n.sub-cat {\n  margin-top: 30px;\n}\n\n.dashboard-card-img {\n  padding: 0 !important;\n}\n\n/* Second row columns */\n.second-row-card {\n  display: flex;\n  flex-direction: column;\n}\n\n.second-row-card h4 {\n  margin-bottom: 1rem;\n}\n\n.second-row-card p {\n  margin-bottom: 0.5rem;\n}\n\n/* Clickable image */\n.img-fluid {\n  width: 100%;\n  height: auto;\n  border-radius: 0.25rem;\n}\n\n/* Responsive styles */\n@media (min-width: 768px) {\n  .second-row-card {\n      flex-direction: row;\n      justify-content: space-between;\n      align-items: center;\n  }\n\n  .second-row-card p {\n      margin-left: 1rem;\n  }\n}\n\n/* Responsive styles */\n@media (min-width: 576px) {\n  .dashboard-title {\n      font-size: 2rem;\n  }\n}\n\n    " }} />
+   
     <title className=''>Dashboard</title>
     {/* CSS styles */}
-  
+   
 
     <div className="container mt-5">
       <h1 className="text-center mb-4 dashboard-title test">Dashboard</h1>
+      
+      <div className="row ">
+      <div className="col-md-6 mb-4">
+          <div className="dashboard-card dashboard-card-img">
+          <YoutubeEmbed embedId="3mrzJ2fKims" />
+          </div>
+        </div> 
+        <div className="col-md-6 mb-4">
+          <div className="dashboard-card dashboard-card-img">
+          <YoutubeEmbed embedId="ffo850LL9Y4" />
+          </div>
+        </div>
+      </div>
       <div className="row">
         {/* First row */}
         <div className="col-12 mb-4">
@@ -173,9 +188,17 @@ let dueAmount=parseInt(totalReceiveBase-statusPaidbase)
           </div>
         </div>
       </div>
+
+   
     </div>
 
-        </>
+ 
+
+
+    
+  
+    
+            </>
        
   );
 };
